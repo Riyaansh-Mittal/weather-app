17ec705465ef96d342e0eb7a985d80e2b9f7bfa2
Riyaansh-Mittal <riyaanshmittal14@gmail.com>
2024-04-08 22:36:23 +0530

    City Search Input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Icon from "react-icons-kit";
+import { search } from "react-icons-kit/feather/search";
+import styled from "styled-components";
 
 function App() {
+  const [loadings, setLoadings] = useState(true);
+  const [city, setCity] = useState("");
+  const handleCitySearch = (e) => {
+    e.preventDefault();
+    setLoadings(true);
+    // fetchData();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Root>
+      <form autoComplete="off" onSubmit={handleCitySearch}>
+        <label>
+          <Icon icon={search} size={20} />
+        </label>
+        <input
+          type="text"
+          className="city-input"
+          placeholder="Enter City"
+          required
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          readOnly={!loadings}
+        />
+        <button type="submit">GO</button>
+      </form>
+    </Root>
   );
 }
 
 export default App;
+
+const Root = styled.div`
+  background: linear-gradient(to bottom, #2fa5ed, #b5d3f9, #ebeefb);
+  background-attachment: fixed;
+  background-size: cover;
+  background-position: center;
+  background-color: #ebeefb;
+  width: 100%;
+  height: 100vh;
+  padding: 5px;
+  form {
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    background-color: #fff;
+    border-radius: 4px;
+    height: 31px;
+    display: flex;
+    align-items: center;
+  }
+  form label {
+    height: 100%;
+    margin-left: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #2fa5ed;
+  }
+
+  form .city-input {
+    width: 100%;
+    outline: none;
+    border: none;
+    text-indent: 15px;
+  }
+
+  form button {
+    height: 100%;
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+    border: none;
+    outline: none;
+    background-color: #fff;
+    color: #2fa5ed;
+    width: 75px;
+    cursor: pointer;
+  }
+`;
